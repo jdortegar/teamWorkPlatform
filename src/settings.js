9b939dcb3f97df4d574979641b475ae0e31aa051
Anthony Daga <anthony@korrelated.com>
2017-07-04 17:27:23 -0700

    Added redux devtools for state live editing/time travel. Linting. Directory structure. Tie routing to redux.

--- src/settings.js
+++
@@ -1,6 +0,0 @@
-export const apiUrl = 'http://api.fixer.io'; // Your main API url here - fixer.io just for testing.
-
-export const apiEndpoints = {
-  testAPIHomepage: `${apiUrl}/latest?base=USD`,
-  testAPISubpage: `${apiUrl}/latest?base=GBP`,
-};
