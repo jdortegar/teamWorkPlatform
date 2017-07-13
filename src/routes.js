6d754518e479422a99da4a258972cfe05618d694
Anthony Daga <anthony@korrelated.com>
2017-07-13 13:20:15 -0700

    Made consistent routing impl (i.e. Main routes within layouts consistent with Signup).  Deep linking challenged (if necessary) forwarded to target.  action type AUTH_USER now has payload 'user' so that user info is in redux store.

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
-import HomeContainer from './containers/Home';
-import Login from './containers/Login';
-import Logout from './components/Logout/Logout';
-import Register from './containers/Register';
-import SubpageContainer from './containers/Subpage';
-import CreateAccount from './containers/CreateAccount';
+import { Route, Switch } from 'react-router-dom';
 import Main from './layouts/Main';
 import SignUp from './layouts/SignUp';
-import Integrations from './containers/user/Integrations';
 import ProtectedRoute from './ProtectedRoute';
 import PublicRoute from './PublicRoute';
 
@@ -24,17 +17,10 @@ export const routesPaths = {
 
 export default (
   <Switch>
-      <Route exact path={routesPaths.register} component={SignUp} />
-      <Route exact path={routesPaths.createAccount} component={SignUp} />
-      <PublicRoute exact path={routesPaths.login} component={SignUp} />
-    <Main>
-      <Switch>
-        <ProtectedRoute exact path={routesPaths.home} component={HomeContainer} />
-        <ProtectedRoute exact path={routesPaths.integrations} component={Integrations} />
-        <ProtectedRoute exact path={routesPaths.subpage} component={SubpageContainer} />
-        <ProtectedRoute exact path={routesPaths.logout} component={Logout} />
-      </Switch>
-    </Main>
+    <Route exact path={routesPaths.register} component={SignUp} />
+    <Route exact path={routesPaths.createAccount} component={SignUp} />
+    <PublicRoute exact path={routesPaths.login} component={SignUp} />
+    <ProtectedRoute component={Main} />
+    <Main />
   </Switch>
 );
-
