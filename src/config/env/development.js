f2637b9da58a7659f8cad49e23ae0d67da7f5b8b
Gonzalo Marquez <gmarquez@mexnetwork.com>
2017-07-26 21:55:00 -0500

    uri

--- src/config/env/development.js
+++ src/config/env/development.js
@@ -1,3 +1,3 @@
-// export const hablaApiBaseUri = 'https://habla-fe-api-dev.habla.ai'; // eslint-disable-line import/prefer-default-export
+export const hablaApiBaseUri = 'https://habla-fe-api-dev.habla.ai'; // eslint-disable-line import/prefer-default-export
 // export const hablaApiBaseUri = 'http://localhost:3000';
-export const hablaApiBaseUri = 'https://hablaapi.ngrok.io';
+// export const hablaApiBaseUri = 'https://hablaapi.ngrok.io';
