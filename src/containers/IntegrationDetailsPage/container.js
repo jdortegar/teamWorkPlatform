e7a2f1869544861a0802839e02a5d2a69c31f821
Gonzalo Marquez <gmarquez@mexnetwork.com>
2017-10-10 21:48:23 -0500

    added integration toast, revokeIntegration

--- src/containers/IntegrationDetailsPage/container.js
+++ src/containers/IntegrationDetailsPage/container.js
@@ -1,6 +1,12 @@
 import { connect } from 'react-redux';
 import IntegrationDetailsPage from '../../pages/IntegrationDetailsPage';
-import { requestIntegrations, integrateBox, integrateGoogle } from '../../actions';
+import {
+  requestIntegrations,
+  integrateBox,
+  integrateGoogle,
+  revokeBox,
+  revokeGoogle
+} from '../../actions';
 
 function mapStateToProps(state) {
   return {
@@ -13,7 +19,9 @@ function mapDispatchToProps(dispatch) {
   return {
     requestIntegrations: subscriberOrgId => dispatch(requestIntegrations(subscriberOrgId)),
     integrateBox: subscriberOrgId => dispatch(integrateBox(subscriberOrgId)),
-    integrateGoogle: subscriberOrgId => dispatch(integrateGoogle(subscriberOrgId))
+    integrateGoogle: subscriberOrgId => dispatch(integrateGoogle(subscriberOrgId)),
+    revokeBox: subscriberOrgId => dispatch(revokeBox(subscriberOrgId)),
+    revokeGoogle: subscriberOrgId => dispatch(revokeGoogle(subscriberOrgId))
   };
 }
 
