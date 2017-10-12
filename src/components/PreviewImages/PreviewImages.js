c4e8dfc7c676c4aff97660fa0561ff88c57cc1dc
DJeanCar <jean_carlos104@hotmail.com>
2017-10-11 19:50:29 -0500

    show image sent in message and replies

--- src/components/PreviewImages/PreviewImages.js
+++ src/components/PreviewImages/PreviewImages.js
@@ -2,42 +2,69 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { getJwt } from '../../session';
 import PropTypes from 'prop-types';
+import { Upload, Icon, Modal } from 'antd';
+import './styles/style.css';
 
 export default class PreviewImages extends Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			image: null,
+			previewVisible: false,
+			previewImage: '',
+			images: [],
 		};
+
+		this.handlePreview = this.handlePreview.bind(this);
 	}
 
 	componentDidMount() {
 		const { images } = this.props;
-		const { resourceId } = images;
 		const putHeaders = {
       headers: {
         Authorization: `Bearer ${getJwt()}`,
       }
     };
-    axios.get(`https://uw33cc3bz4.execute-api.us-west-2.amazonaws.com/dev/resource/${resourceId}`, putHeaders)
-    .then(resource => {
-      // console.log(resource.data);
-      const imageBase64 = resource.data.split("base64")[1];
-      console.log(resource);
-      const imageSrc = `data:image/png;base64,${imageBase64}`;
-      this.setState({
-      	image: imageSrc
-      });
-    });
+    const imagesBase64 = [];
+		images.forEach(image => {
+			const { resourceId } = image;
+			axios.get(`https://uw33cc3bz4.execute-api.us-west-2.amazonaws.com/dev/resource/${resourceId}`, putHeaders)
+				.then(resource => {
+					const imageBase64 = resource.data.split("base64")[1];
+					const imageSrc = `data:image/png;base64,${imageBase64}`;
+					imagesBase64.push(imageSrc);
+					this.setState({
+						images: imagesBase64,
+					});
+				});
+		});
+	}
+
+	handleCancel() {
+		this.setState({ 
+			previewVisible: false,
+		});
+	}
+
+	handlePreview(image) {
+		this.setState({
+			previewVisible: true,
+			previewImage: image,
+		});
 	}
 
 	render() {
-		const { image } = this.state;
-		// console.log(image);
+		const { images, previewVisible, previewImage } = this.state;
 		return (
-			<div>
-			{ image && <img src={image} /> }
+			<div className="preview-images">
+			{ images.map((image, index) => 
+				<div className="image-wrapper" key={index}>
+					<img src={image} onClick={() => this.handlePreview(image)} />
+				</div>
+			)}
+			<Modal visible={previewVisible} footer={null} onCancel={this.handleCancel.bind(this)}>
+				<img style={{ width: '100%' }} src={previewImage} />
+			</Modal>
 			</div>
 		);
 	}
@@ -45,6 +72,6 @@ export default class PreviewImages extends Component {
 }
 
 PreviewImages.propTypes = {
-	images: PropTypes.object
+	images: PropTypes.array
 };
 
