9def120c39bb02756573ade4b7b1a80e6c13dd8e
DJeanCar <jean_carlos104@hotmail.com>
2017-10-18 15:41:26 -0500

    remove console log

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -10,7 +10,6 @@ import './styles/style.css';
 export default class Message extends Component {
   constructor(props) {
     super(props);
-    // console.log(props);
 
     this.state = {
       mute: true,
