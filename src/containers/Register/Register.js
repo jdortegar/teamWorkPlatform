5d4e54bf50a00dc4d796aaa80f500cd6f3a32d48
Habla-Mike <mikesomlo@gmail.com>
2017-12-11 19:16:50 -0800

    Changed Register screen to add checkbox, text, links to open Privacy Policy and Terms of Use Agreement, and cancel button.

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Form, Button, Spin, Icon } from 'antd';
+import PropTypes from 'prop-types';
+import { Form, Button, Spin, Icon, Checkbox } from 'antd';
 import axios from 'axios';
 import { formShape } from '../../propTypes';
 import config from '../../config/env';
@@ -11,7 +12,8 @@ import String from '../../translations';
 const FormItem = Form.Item;
 
 const propTypes = {
-  form: formShape.isRequired
+  form: formShape.isRequired,
+  history: PropTypes.object.isRequired
 };
 
 const layout = {
@@ -23,9 +25,20 @@ class Register extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { submitting: false, registered: false };
+    this.state = { submitting: false, registered: false, agreementsChecked: false };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.onCancel = this.onCancel.bind(this);
+    this.onCheckboxChange = this.onCheckboxChange.bind(this);
   }
+
+  onCancel() {
+    this.props.history.replace('/app');
+  }
+
+  onCheckboxChange() {
+    this.setState({ agreementsChecked: !this.state.agreementsChecked });
+  }
+
   handleSubmit(e) {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
@@ -45,21 +58,54 @@ class Register extends React.Component {
     });
   }
 
-  renderButton() {
-    if (!this.state.submitting && this.state.registered) {
-      return (
-        <div className="registration-success">
-          <div><Icon type="check-circle-o" /></div>
-          <h2 style={{ textAlign: 'center' }}>{String.t('register.successText')}</h2>
-        </div>
-      );
-    } else if (this.state.submitting) {
+  renderRegistered() {
+    if (this.state.submitting) {
       return <Spin size="large" style={{ width: '100%' }} />;
     }
+    return (
+      <div className="registration-success">
+        <div><Icon type="check-circle-o" /></div>
+        <h2 style={{ textAlign: 'center' }}>{String.t('register.successText')}</h2>
+      </div>
+    );
+  }
 
+  renderPreRegisteredButtons() {
+    const buttonBackgroundColor = this.state.agreementsChecked ? '#00a854' : '#aac8a4';
     return (
       <FormItem>
-        <Button type="primary" htmlType="submit" className="login-form-button">
+        <div className="register-checkbox-div">
+          <Checkbox
+            checked={this.state.agreementsChecked}
+            onChange={this.onCheckboxChange}
+          >
+            {String.t('register.checkAgreementsLabelBeforePrivacyPolicy')}
+            <a
+              onClick={() => window.open('https://habla.ai/privacy-policy.html')}
+              className="register-link"
+            >
+              {String.t('register.checkAgreementsPrivacyPolicyLink')}
+            </a>
+            {String.t('register.checkAgreementsLabelBeforeTermsOfUse')}
+            <a
+              onClick={() => window.open('https://habla.ai/user-terms-of-service.html')}
+              className="register-link"
+            >
+              {String.t('register.checkAgreementsTermsOfUseLink')}
+            </a>
+            {String.t('register.checkAgreementsLabelAfterTermsOfUse')}
+          </Checkbox>
+        </div>
+        <Button type="primary" className="form-cancel-button" onClick={this.onCancel}>
+          {String.t('cancelButton')}
+        </Button>
+        <Button
+          disabled={!this.state.agreementsChecked}
+          type="primary"
+          htmlType="submit"
+          className="form-action-button"
+          style={{ backgroundColor: buttonBackgroundColor, borderColor: buttonBackgroundColor }}
+        >
           {String.t('register.registerButtonLabel')}
         </Button>
       </FormItem>
@@ -67,15 +113,29 @@ class Register extends React.Component {
   }
 
   render() {
+    if (this.state.registered) {
+      return this.renderRegistered();
+    }
+
     return (
-      <Form onSubmit={this.handleSubmit} layout="vertical">
-        <EmailField
-          form={this.props.form}
-          layout={layout}
-          required
-        />
-        { this.renderButton() }
-      </Form>
+      <div>
+        <div className="register-title-div">
+          <span className="register-title-bold">{String.t('register.titleBold')}
+            <span className="register-title-normal">{String.t('register.titleDetails')}</span>
+          </span>
+        </div>
+        <Form onSubmit={this.handleSubmit} layout="vertical">
+          <EmailField
+            form={this.props.form}
+            layout={layout}
+            placeholder={String.t('register.emailPlaceholder')}
+            noLabel
+            required
+          />
+          <div style={{ height: 10 }} />
+          { this.renderPreRegisteredButtons() }
+        </Form>
+      </div>
     );
   }
 }
