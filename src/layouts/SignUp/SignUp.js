943722023da50f0b59ea4a2c8c29a9a37a5c35a8
Habla-Mike <mikesomlo@gmail.com>
2017-12-11 18:26:45 -0800

    Added Header to SignUp() and removed the logo and company name.

--- src/layouts/SignUp/SignUp.js
+++ src/layouts/SignUp/SignUp.js
@@ -4,27 +4,22 @@ import { Row, Col } from 'antd';
 import { routesPaths } from '../../routes';
 import Register from '../../containers/Register';
 import CreateAccount from '../../containers/CreateAccount';
-import Login from '../../containers/Login';
 import VerifyAccount from '../../containers/VerifyAccount';
+import Header from '../../components/Header';
 import './styles/signup.css';
-import String from '../../translations';
 
 // TODO: add <Route exact path={routesPaths.verifyAccount} component={VerifyAccount} /> below.
 function SignUp() {
   // TODO: need VerifyAccount component.
   return (
     <div className="signup-main-div">
-      <Row type="flex" justify="center" align="middle" style={{ width: '100%' }}>
+      <Header />
+      <Row type="flex" justify="center" align="middle">
         <Col xs={{ span: 20 }} md={{ span: 12 }} lg={{ span: 8 }}>
-          <div style={{ textAlign: 'center' }}>
-            <img alt={String.t('signUp.logoAlt')} className="signup-logo" src="https://c2.staticflickr.com/4/3955/33078312014_f6f8c759db_o.png" />
-            <h2>{String.t('signUp.companyName')}</h2>
-          </div>
           <Switch>
             <Route exact path={routesPaths.register} component={Register} />
             <Route exact path={routesPaths.createAccount} component={CreateAccount} />
             <Route exact path={routesPaths.verifyAccount} component={VerifyAccount} />
-            <Route path={routesPaths.login} component={Login} />
           </Switch>
         </Col>
       </Row>
