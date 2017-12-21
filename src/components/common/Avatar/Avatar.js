507a1afbe006a4c630e05a97662da39be56bef39
Gonzalo <marquezgon@me.com>
2017-12-21 12:23:15 -0600

    added avatar to reusable components, updated antd and react

---
+++ src/components/common/Avatar/Avatar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { Avatar as AntdAvatar } from 'antd';
+import './styles/style.css';
+
+const propTypes = {
+  ...AntdAvatar.propTypes,
+  size: PropTypes.oneOf(['large', 'small', 'default']),
+  color: PropTypes.string
+};
+
+const defaultProps = {
+  size: 'default',
+  color: '#ccc'
+};
+
+const CLASSNAME = 'habla-avatar';
+
+function Avatar(props) {
+  const { children, size, color, className: userClassName, ...rest } = props;
+  const className = classNames(
+    CLASSNAME,
+    userClassName,
+    `${CLASSNAME}--size-${size}`
+  );
+
+  return (
+    <AntdAvatar
+      size={size}
+      className={className}
+      style={{ backgroundColor: color }}
+      {...rest}
+    >
+      {children}
+    </AntdAvatar>
+  );
+}
+
+Avatar.propTypes = propTypes;
+Avatar.defaultProps = defaultProps;
+export default Avatar;
