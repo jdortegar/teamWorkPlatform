b426c59cb78753ac62149d21891d79aaeaf844fe
Josias Schneider <josias.ds@gmail.com>
2017-12-27 18:29:28 -0200

    Get team room members presence when logging in

--- src/redux-hablaai/reducers/presencesReducer.js
+++ src/redux-hablaai/reducers/presencesReducer.js
@@ -1,26 +1,62 @@
 import _ from 'lodash';
 import {
-  PRESENCE_CHANGE
+  PRESENCE_CHANGE,
+  TEAMROOMMEMBER_RECEIVE,
+  TEAMROOMMEMBERS_FETCH_SUCCESS
 } from '../actions';
 
 const INITIAL_STATE = {
   presencesByUserId: {}
 };
 
+// Uniqueness by address + userAgent combination.
+const buildPresenceUniqueId = ({ address, userAgent }) => `${address}##${userAgent}`;
+
 const presencesReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case PRESENCE_CHANGE: {
       const presencesByUserId = _.cloneDeep(state.presencesByUserId);
       const { presence } = action.payload;
-      let presences = presencesByUserId[presence.userId];
-      const uniqueness = `${presence.address}##${presence.userAgent}`; // Uniqueness by address + userAgent combination.
 
-      if (!presences) {
-        presences = {};
-        presencesByUserId[presence.userId] = presences;
-      }
+      presencesByUserId[presence.userId] = {
+        ...presencesByUserId[presence.userId],
+        [buildPresenceUniqueId(presence)]: presence
+      };
 
-      presences[uniqueness] = presence;
+      return {
+        ...state,
+        presencesByUserId
+      };
+    }
+    case TEAMROOMMEMBERS_FETCH_SUCCESS: {
+      const presencesByUserId = _.cloneDeep(state.presencesByUserId);
+      const { teamRoomMembers } = action.payload;
+
+      teamRoomMembers.forEach(({ presence, userId }) => (
+        presence.forEach((item) => {
+          presencesByUserId[userId] = {
+            ...presencesByUserId[userId],
+            [buildPresenceUniqueId(item)]: item
+          };
+        })
+      ));
+
+      return {
+        ...state,
+        presencesByUserId
+      };
+    }
+    case TEAMROOMMEMBER_RECEIVE: {
+      const presencesByUserId = _.cloneDeep(state.presencesByUserId);
+      const { presence = [], userId } = action.payload.teamRoomMember;
+
+      presence.forEach((item) => {
+        presencesByUserId[userId] = {
+          ...presencesByUserId[userId],
+          [buildPresenceUniqueId(item)]: item
+        };
+      });
+
       return {
         ...state,
         presencesByUserId
