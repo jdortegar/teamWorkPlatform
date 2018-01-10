839c77ca65ed2018d487a0b4e9f254528f6a1830
Josias Schneider <josias.ds@gmail.com>
2018-01-10 15:50:56 -0200

    Render data points from API in the chart

--- src/components/TimeActivityGraph/TimeActivityGraph.js
+++ src/components/TimeActivityGraph/TimeActivityGraph.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { VictoryChart, VictoryScatter, VictoryTheme } from 'victory';
+import {
+  VictoryChart,
+  VictoryScatter,
+  VictoryTheme
+} from 'victory';
 
 import './styles/style.css';
 
@@ -13,22 +17,14 @@ const defaultProps = {
 };
 
 const TimeActivityGraph = ({ files }) => {
-  console.warn('FILES:', files);
   return (
-    <VictoryChart
-      theme={VictoryTheme.material}
-      domain={{ x: [0, 5], y: [0, 7] }}
-    >
+    <VictoryChart theme={VictoryTheme.material}>
       <VictoryScatter
         style={{ data: { fill: '#c43a31' } }}
-        size={6}
-        data={[
-          { x: 1, y: 2 },
-          { x: 2, y: 3 },
-          { x: 3, y: 5 },
-          { x: 4, y: 4 },
-          { x: 5, y: 7 }
-        ]}
+        size={3}
+        data={files}
+        x={'date'}
+        y={'time'}
       />
     </VictoryChart>
   );
