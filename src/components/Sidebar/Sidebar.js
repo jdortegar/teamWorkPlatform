d3840240fc88e66a57fa5b198994991ae44ee2bc
Gonzalo <marquezgon@me.com>
2018-01-10 12:02:03 -0600

    avatar now obtains color background from iconColor in preferences object

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -43,19 +43,13 @@ const ROUTERS_TO_HIDE_SIDEBAR = [
   '/app/userDetails'
 ];
 
-function renderAvatar(item, type) {
+function renderAvatar(item) {
   const { preferences } = item;
-  let color = '#EB4435'; // subscriberOrg color by default
-  if (type === 'teamRoom') {
-    color = '#557DBF';
-  } else if (type === 'team') {
-    color = '#FBBC12';
-  }
   if (preferences.avatarBase64) {
     return <Avatar src={`data:image/jpeg;base64, ${preferences.avatarBase64}`} />;
   }
   const nameInitial = item.name.substring(0, 1).toUpperCase();
-  return <Avatar color={color}>{nameInitial}</Avatar>;
+  return <Avatar color={preferences.iconColor}>{nameInitial}</Avatar>;
 }
 
 class Sidebar extends Component {
@@ -203,7 +197,7 @@ class Sidebar extends Component {
         <div className="habla-left-navigation-teamroom-list">
           <div className="padding-class-a">
             <div className="float-left-class">
-              {renderAvatar(teamRoom, 'teamRoom')}
+              {renderAvatar(teamRoom)}
             </div>
             <span className="habla-left-navigation-item-label" onClick={() => this.goToTeamRoomPage(teamRoom.teamRoomId)}>{teamRoom.name}</span>
             <div className="clear" />
@@ -237,7 +231,7 @@ class Sidebar extends Component {
             <div className="habla-left-navigation-team-list">
               <div className="padding-class-a">
                 <div className="float-left-class">
-                  {renderAvatar(team, 'team')}
+                  {renderAvatar(team)}
                 </div>
                 <span className="habla-left-navigation-item-label" onClick={e => this.goToTeamPage(e, team)}>{team.name}</span>
                 <div className="clear" /></div>
@@ -269,7 +263,7 @@ class Sidebar extends Component {
             <div className="habla-left-navigation-org-list">
               <div className="padding-class-a">
                 <div className="float-left-class">
-                  {renderAvatar(subscriberOrg, 'subscriberOrg')}
+                  {renderAvatar(subscriberOrg)}
                 </div>
                 <span className="habla-left-navigation-item-label" onClick={e => this.goToOrgPage(e, subscriberOrg.subscriberOrgId)}>{subscriberOrg.name}</span>
                 <div className="clear" /></div>
