abe78247e3790688eb573d81d46eff647b2da00f
Gonzalo <marquezgon@me.com>
2018-01-25 12:50:37 -0600

    switched style loader and css loader in webpack prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -63,10 +63,7 @@ config.module.rules = [{
   ]
 }, {
   test: /\.css$/,
-  use: ExtractTextPlugin.extract({
-    fallback: 'style-loader',
-    use: 'css-loader'
-  })
+  use: ['css-loader', 'style-loader']
 }, {
   test: /\.(woff|woff2|eot|ttf|otf|jpg|png|svg|mp3)$/,
   exclude: /(node_modules)/,
