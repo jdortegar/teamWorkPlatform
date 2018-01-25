cd419612c52b3f5161eb359aa99c64fc8988dd51
Gonzalo <marquezgon@me.com>
2018-01-25 12:33:09 -0600

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 let nodeUrl = process.env.NODE_URL;
 if (!nodeUrl) {
