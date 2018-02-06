13a8ba34968b1b69aaabed9c8cb7bb56c05e586d
Gonzalo <marquezgon@me.com>
2018-02-06 11:23:46 -0600

    made dev default env

--- src/config/env/index.js
+++ src/config/env/index.js
@@ -2,7 +2,7 @@ import * as dev from './development';
 
 const config = {
   hablaApiBaseUri: process.env.HABLAAPI_BASE_URI || dev.hablaApiBaseUri,
-  hablaApiEnv: process.env.HABLAAPI_ENV || 'prod'
+  hablaApiEnv: process.env.HABLAAPI_ENV || 'dev'
 };
 
 export default config;
