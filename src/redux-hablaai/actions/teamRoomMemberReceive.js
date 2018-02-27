7942ce68988ea6a39dc67aa584183a5a42b5244a
Habla-Mike <mikesomlo@gmail.com>
2018-02-26 22:31:26 -0800

    Partial fix Redux handling of new team/room member notification.

--- src/redux-hablaai/actions/teamRoomMemberReceive.js
+++ src/redux-hablaai/actions/teamRoomMemberReceive.js
@@ -1,23 +1,10 @@
-import { fetchTeamRooms } from './teamRoomsFetch';
-
 export const TEAMROOMMEMBER_RECEIVE = 'teamroommember/receive';
 
 export const receiveTeamRoomMember = (teamRoomMember, teamRoomId) => {
-  return ((dispatch, getState) => {
-    const teamRoom = getState().teamRooms.teamRoomById[teamRoomId];
-    if (!teamRoom) {
-      dispatch(fetchTeamRooms({ forceGet: true }))
-        .then(() => {
-          dispatch({
-            type: TEAMROOMMEMBER_RECEIVE,
-            payload: { teamRoomMember, teamRoomId }
-          });
-        });
-    } else {
-      dispatch({
-        type: TEAMROOMMEMBER_RECEIVE,
-        payload: { teamRoomMember, teamRoomId }
-      });
-    }
+  return ((dispatch) => {
+    dispatch({
+      type: TEAMROOMMEMBER_RECEIVE,
+      payload: { teamRoomMember, teamRoomId }
+    });
   });
 };
