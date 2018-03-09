4dc6008a498b959bbe14ca39cb1a2ad84120f846
Josias Schneider <josias.ds@gmail.com>
2018-03-09 14:14:17 -0300

    Fetch teams when accepting org invitations (#57)
    * Fetch teams when accepting org invitations
    
    * Fetch team rooms when accepting org invitation

--- src/containers/Main/container.js
+++ src/containers/Main/container.js
@@ -2,16 +2,12 @@ import { connect } from 'react-redux';
 import Main from 'layouts/Main';
 import { fetchGlobalState } from 'actions';
 
-function mapStateToProps(state) {
-  return {
-    subscriberOrgs: state.subscriberOrgs
-  };
-}
+const mapStateToProps = state => ({
+  subscriberOrgs: state.subscriberOrgs
+});
 
-function mapDispatchToProps(dispatch) {
-  return {
-    fetchGlobalState: () => dispatch(fetchGlobalState())
-  };
-}
+const mapDispatchToProps = dispatch => ({
+  fetchGlobalState: () => dispatch(fetchGlobalState())
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
