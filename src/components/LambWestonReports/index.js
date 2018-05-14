82550ef3e9483a0580ae5d8abf21e85853e3b6a1
Jonatan Frank <jonafrank@gmail.com>
2018-05-14 17:49:51 -0300

    [HAB-1124] Lamb Weston Report C

--- src/components/LambWestonReports/index.js
+++ src/components/LambWestonReports/index.js
@@ -1,9 +1,11 @@
 import DowntimeAndReasonsLevelOne from './DowntimeAndReasonsLevelOne';
 import DailyPlantUptimeByLineAndString from './DailyPlantUptimeByLineAndString';
 import PlantUptimeByLineAndString from './PlantUptimeByLineAndString';
+import PlanUpMultipleComparissions from './PlanUpMultipleComparissions';
 
 export default {
   DowntimeAndReasonsLevelOne,
   DailyPlantUptimeByLineAndString,
-  PlantUptimeByLineAndString
+  PlantUptimeByLineAndString,
+  PlanUpMultipleComparissions
 };
