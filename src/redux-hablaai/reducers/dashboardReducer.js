fe8c73e2f591973ecc17590fc9d278afc90b1aea
Josias Schneider <josias.ds@gmail.com>
2018-06-08 14:02:20 -0300

    Fix dashboard reducers updates

--- src/redux-hablaai/reducers/dashboardReducer.js
+++ src/redux-hablaai/reducers/dashboardReducer.js
@@ -1,5 +1,3 @@
-import _ from 'lodash';
-
 import {
   LWREPORTS_PLANTUPTIME_FETCH_SUCCESS,
   LWREPORTS_PLANTUPTIMEMULTIPLE_FETCH_SUCCESS
@@ -34,15 +32,32 @@ const dashboardReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case LWREPORTS_PLANTUPTIME_FETCH_SUCCESS: {
       const { categories, series, measure } = action.payload;
-      return _.merge(state, {
-        reports: { plantUptime: { categories, series, measure } }
-      });
+      return {
+        ...state,
+        reports: {
+          ...state.reports,
+          plantUptime: {
+            ...state.reports.plantUptime,
+            categories,
+            series,
+            measure
+          }
+        }
+      };
     }
     case LWREPORTS_PLANTUPTIMEMULTIPLE_FETCH_SUCCESS: {
       const { series, measure } = action.payload;
-      return _.merge(state, {
-        reports: { plantUptimeMultiple: { series, measure } }
-      });
+      return {
+        ...state,
+        reports: {
+          ...state.reports,
+          plantUptimeMultiple: {
+            ...state.reports.plantUptimeMultiple,
+            series,
+            measure
+          }
+        }
+      };
     }
     default:
       return state;
