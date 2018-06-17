71f1f7791a16b6e7d4188650586d88deeb34defa
Kelly Qian <kqian@Kellys-MBP.home.maxqia.net>
2018-06-16 17:16:03 -0700

    more Fixed GraphAPI issue

--- src/redux-hablaai/actions/timeActivitiesFetch.js
+++ src/redux-hablaai/actions/timeActivitiesFetch.js
@@ -4,7 +4,7 @@ import config from '../../config/env';
 export const TIMEACTIVITIES_FETCH_SUCCESS = 'timeactivities/fetch/success';
 
 export const fetchTimeActivitiesBySubscriberOrgId = (subscriberOrgId, options = { getKey: false, forceGet: true }) => {
-  const requestUrl = `${config.hablaApiBaseUri}/v1/ckg/getFiles/${subscriberOrgId}`;
+  const requestUrl = `${config.hablaApiBaseUri}/ckg/getFiles/${subscriberOrgId}`;
 
   // Passthrough data that you'll see after going through the reducer.  Typically in you mapStateToProps.
   const reduxState = { subscriberOrgId };
