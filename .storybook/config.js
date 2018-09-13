109393b4534275537761aed00e6e7d4c3356329d
Josias Schneider <josias.ds@gmail.com>
2018-09-13 16:37:49 -0300

    Add storybook for UI development (#127)
    * Add storybook for UI development
    * Add Button and Toast stories

---
+++ .storybook/config.js
@@ -0,0 +1,12 @@
+import { configure } from '@storybook/react';
+import { addDecorator } from '@storybook/react';
+import { withConsole } from '@storybook/addon-console';
+import { withKnobs } from '@storybook/addon-knobs/react';
+
+addDecorator((storyFn, context) => withConsole()(storyFn)(context));
+addDecorator(withKnobs);
+
+const req = require.context('../src/components', true, /\.stories\.js$/);
+const loadStories = () => req.keys().forEach(filename => req(filename));
+
+configure(loadStories, module);
