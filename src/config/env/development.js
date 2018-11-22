86eb7626580d7caf225b511415bae273ec9683e0
Jonatan Frank <jonafrank@gmail.com>
2018-11-22 16:27:36 -0300

    rollback

--- src/config/env/development.js
+++ src/config/env/development.js
@@ -1,3 +1,3 @@
-// export const API_ROOT = 'https://habla-fe-api-sjdev.habla.ai';
-export const API_ROOT = 'http://localhost:3000';    
+export const API_ROOT = 'https://habla-fe-api-sjdev.habla.ai';
+// export const API_ROOT = 'http://localhost:3000';    
 export const KNOWLEDGE_API_ROOT = 'https://habla-be-api-dev.habla.ai';
