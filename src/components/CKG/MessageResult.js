5bac7a405815fb2a225889560cb82aa0c89ab9c0
Josias Schneider <josias.ds@gmail.com>
2019-04-03 13:32:53 -0300

    [HAB-2137] Add team name to message results

--- src/components/CKG/MessageResult.js
+++ src/components/CKG/MessageResult.js
@@ -11,15 +11,17 @@ import './styles/MessageResult.css';
 const propTypes = {
   message: PropTypes.object.isRequired,
   history: PropTypes.object.isRequired,
+  team: PropTypes.object,
   sender: PropTypes.object
 };
 
 const defaultProps = {
-  sender: {}
+  sender: {},
+  team: null
 };
 
-const MessageResult = ({ message, sender, history }) => {
-  const { content, created, teamId, conversationId } = message;
+const MessageResult = ({ message, sender, team, history }) => {
+  const { content, created, conversationId } = message;
   const { firstName, lastName } = sender;
   const name = String.t('message.sentByName', { firstName, lastName });
   const { text = '' } = find(content, { type: 'text/plain' }) || {};
@@ -27,10 +29,11 @@ const MessageResult = ({ message, sender, history }) => {
   const attachments = filter(content, ({ type }) => type !== 'text/plain');
 
   return (
-    <div className="MessageResult" onClick={() => history.push(`/app/team/${teamId}`)}>
+    <div className="MessageResult" onClick={() => team && history.push(`/app/team/${team.teamId}`)}>
       <AvatarWrapper user={sender} />
       <div className="MessageResult__content">
         <p className="MessageResult__sender">
+          {team && `#${team.name} - `}
           {name} - {moment(created).fromNow()}
         </p>
         <p className="MessageResult__text">
--- src/containers/MessageResult/container.js
+++ src/containers/MessageResult/container.js
@@ -1,11 +1,12 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
-import { getUserById } from 'src/selectors';
+import { getUserById, getTeam } from 'src/selectors';
 import { MessageResult } from 'src/components';
 
 const mapStateToProps = (state, props) => ({
-  sender: getUserById(state, props.message.createdBy)
+  sender: getUserById(state, props.message.createdBy),
+  team: getTeam(state, props.message.teamId)
 });
 
 export default withRouter(connect(mapStateToProps)(MessageResult));
