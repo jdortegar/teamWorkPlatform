a0466d15a99c3bccc7dc393f82578c98a81e0fbb
Josias Schneider <josias.ds@gmail.com>
2019-04-08 11:59:48 -0300

    [HAB-2145] Fix redirect issue

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 import { store, persistor, history } from './store';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <PersistGate persistor={persistor} loading={null}>
-      <App store={store} persistor={persistor} history={history} />
-    </PersistGate>
-  </BrowserRouter>,
-  document.getElementById('app')
-);
+ReactDOM.render(<App store={store} persistor={persistor} history={history} />, document.getElementById('app'));
