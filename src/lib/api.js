d63282194f6cb4c9293da062f04efc80466a281a
Josias Schneider <josias.ds@gmail.com>
2019-04-10 14:09:42 -0300

    [HAB-2041] Configure chat api

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,3 +2,4 @@ import config from 'src/config/env';
 
 export const buildApiUrl = (endpoint, version = 'v1') => `${config.apiRoot}/${version}/${endpoint}`;
 export const buildKnowledgeApiUrl = endpoint => `${config.knowledgeApiRoot}/${endpoint}`;
+export const buildChatUrl = (endpoint, version = 'v1') => `${config.chatApiRoot}/${version}/${endpoint}`;
