66058cc86bd0a60641d44f5de58a070a4e4ce299
Josias Schneider <josias.ds@gmail.com>
2019-05-14 15:27:33 -0300

    [HAB-2243] Add userId to typing event (#294)

--- src/redux-hablaai/actions/typing.js
+++ src/redux-hablaai/actions/typing.js
@@ -1,3 +1,4 @@
+import { getCurrentUserId } from 'src/selectors';
 import { apiMessaging } from '../messaging';
 
 export const TYPING_RECEIVE = 'typing/receive';
@@ -9,9 +10,10 @@ export const receiveTyping = ({ userId, conversationId, isTyping }) => ({
 
 /**
  * @param conversationId
- * @param typing True if the user is typing in the specified conversation, or false if the user is not.
- * Typically, this is some amount of time (ex. 15 seconds) after the last key press.
+ * @param isTyping True if the user is typing in the specified conversation, or false if the user is not.
+ * Typically, this is some amount of time (ex. 5 seconds) after the last key press.
  */
-export const sendTyping = (conversationId, typing) => () => {
-  apiMessaging().sendTyping(conversationId, typing);
+export const sendTyping = (conversationId, isTyping) => (dispatch, getState) => {
+  const userId = getCurrentUserId(getState());
+  apiMessaging().sendTyping({ conversationId, userId, isTyping });
 };
